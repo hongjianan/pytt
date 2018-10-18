e2543ed428578722eda43bab1ed66f058668cbda
hongjianan <hjnhong@163.com>
2018-10-18 21:51:41 +0800

    [ADD] tk

--- src/tkinter/tk_test.py
+++ src/tkinter/tk_test.py
@@ -4,10 +4,21 @@ import Tkinter
 
 
 def gui():
-    top = Tkinter.Tk()
-    label = Tkinter.Label(top, text = "Hello World.")
-    button = Tkinter.Button(top, text = "quit", command = top.quit, bg = "red", fg = "white")
+    root = Tkinter.Tk()
+    # title
+    root.title('工具')
+    # size
+    root.geometry('600x700')
+    # stable 宽不可变, 高可变,默认为True
+    # root.resizable(width=False, height=True)
+    
+    listbox = Tkinter.Listbox(root)
+    listbox.geometry('600x700')
 
+    label = Tkinter.Label(root, text = "Hello World.")
+    button = Tkinter.Button(root, text = "quit", command = root.quit, bg = "red", fg = "white")
+    
+    listbox.pack()
     label.pack()
     button.pack(fill = Tkinter.X, expand = 1)
     Tkinter.mainloop()
