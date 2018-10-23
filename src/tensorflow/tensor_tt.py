7fbbe5ce927914f02d37c09cbeb8cbf7a6fdfc71
hongjianan <hjnhong@163.com>
2018-10-24 01:43:13 +0800

    [ADD] tensor variable

--- src/tensorflow/tensor_tt.py
+++ src/tensorflow/tensor_tt.py
@@ -8,13 +8,31 @@ Created on 2018年10月22日
 import tensorflow as tf
 
 
-def tensor_tt():
+def variable_tt():
+    x = tf.Variable([1, 2])
+    a = tf.constant([2, 3])
+    op_sub = a - x
+    op_add = a + x
+    
+    # init
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        print(sess.run(op_sub))
+        print(sess.run(op_add))
+
+def constant_tt():
     a = tf.constant([1, 2], name="a", dtype=tf.float32)
     b = tf.constant([2.0, 3.0], name="b")
     c = a + b
     print(c)
+    
+    with tf.Session() as sess:
+        result = sess.run(c)
+        print(result)
 
 
 if __name__ == '__main__':
-    import pdb; pdb.set_trace()
-    tensor_tt()
+#     import pdb; pdb.set_trace()
+#     constant_tt()
+    variable_tt()
